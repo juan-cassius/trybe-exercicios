8845ab0b0ae864a44e44c4b45b6026019e9a0936
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-01-23 14:21:45 -0300

    Começo dos exercícios do course do dia

---
+++ 01-fundamentos/secao-03-javascript-introducao-a-javascript/scripts.js
@@ -0,0 +1,40 @@
+// Esse é o exercício de variáveis
+const birthCity = "Castro";
+const myName = "Juan Cassius";
+let birthYear = 1997;
+
+// console.log("Nasci em ",birthCity," e meu nome é ",myName,". Nasci no ano de ",birthYear,".");
+
+// myName = 25; Não da certo porque constante não pode ter seu valor alterado. (TypeError: Assignment to constant variable.)
+
+// console.log(myName);
+
+// Esse é o exercício de Tipos
+let movie = "Avengers"; //string literal
+let score = 10.89; //number literal
+let isValid = true; //boolean
+let variavel; //undefined
+let color = null; //redefinição
+
+//+, -, *, /, **
+
+let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(3 ** 3);
+
+//inrementar variável ++
+// decrementar variável --
+// Tem uma diferença de colocar esse tipo de incremento/decremento antes da variável e depoiss
+// exemplo: ++salary e salary++
+// += ou -= fazem com que a variável receba o valor dela mesma mais/menos 1.
+
+//Exercício de tipos
+const base = 5;
+const heigth = 8;
+const area = base*heigth;
+const perimeter = 2*5 + 2*8;
+
+console.log(perimeter);
+console.log(area);
+
