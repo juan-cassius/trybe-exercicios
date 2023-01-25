c95e4423a26c5b9d75de98294af5d7b03da8829f
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-01-25 15:01:30 -0300

    Exercícios 1 e 2 terminados

---
+++ 01-fundamentos/secao-03-javascript-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmos/exercicio-01.js
@@ -0,0 +1,6 @@
+let fat = 3;
+let result = 0;
+for (let index = 1; index < fat; index++) {
+  result = index * (index + 1);
+}
+console.log(result);
---
+++ 01-fundamentos/secao-03-javascript-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritmos/exercicio-02.js
@@ -0,0 +1,7 @@
+let word = "tryber";
+let auxWord = "";
+
+for (let index = word.length-1; index >= 0; index--) {
+    auxWord=auxWord+word[index];
+  }
+console.log(auxWord);
