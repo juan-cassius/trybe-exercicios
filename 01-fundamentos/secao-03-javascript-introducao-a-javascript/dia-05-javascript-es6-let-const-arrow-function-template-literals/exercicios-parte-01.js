87787a052fa56bdde58dc19ab11f887f4ef15669
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-01-27 18:48:11 -0300

    Vou começar o exercício do dia no repo dele

--- 01-fundamentos/secao-03-javascript-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-function-template-literals/exercicios-parte-01.js
+++ 01-fundamentos/secao-03-javascript-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-function-template-literals/exercicios-parte-01.js
@@ -46,10 +46,10 @@
 // let sobrenome = "Pires";
 // console.log(nomeCompleto(nome, sobrenome));
 
-let speed = 90;
-const speedCar = (speed) => {
-  speed >= 120
-    ? `Você ultrapassou o limite de velocidade`
-    : `Você está na velocidade permitida`;
-};
-console.log(speedCar(speed));
+// let speed = 90;
+// const speedCar = (speed) => (
+//   speed >= 120
+//     ? `Você ultrapassou o limite de velocidade`
+//     : `Você está na velocidade permitida`
+// );
+// console.log(speedCar(speed));
