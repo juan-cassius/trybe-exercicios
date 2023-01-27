58fbe1afd5258f4fc7ac72371ebb88a750b279b6
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-01-27 12:11:00 -0300

    Terminei o último, só partir pro abraço

---
+++ 01-fundamentos/secao-03-javascript-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-bonus-04.js
@@ -0,0 +1,33 @@
+let moradores = {
+    blocoUm: [
+      {
+        nome: 'Luiza',
+        sobrenome: 'Guimarães',
+        andar: 10,
+        apartamento: 1005,
+      },
+      {
+        nome: 'William',
+        sobrenome: 'Albuquerque',
+        andar: 5,
+        apartamento: 502,
+      },
+    ],
+    blocoDois: [
+      {
+        nome: 'Murilo',
+        sobrenome: 'Ferraz',
+        andar: 8,
+        apartamento: 804,
+      },
+      {
+        nome: 'Zoey',
+        sobrenome: 'Brooks',
+        andar: 1,
+        apartamento: 101,
+      },
+    ],
+  };
+  let moradoresBlocoDois = moradores.blocoDois;
+  let ultimoMoradorBlocoDois = moradoresBlocoDois[moradoresBlocoDois.length-1]
+  console.log('O morador do bloco 2 de nome '+ultimoMoradorBlocoDois.nome +' mora no '+ultimoMoradorBlocoDois.andar+'° andar, apartamento '+ ultimoMoradorBlocoDois.apartamento+'.');
