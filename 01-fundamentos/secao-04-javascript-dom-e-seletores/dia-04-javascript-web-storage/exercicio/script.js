575fe0888a8487036c11bb791c5e6de906e2c88c
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-02-04 17:25:16 -0300

    Aprendi a usar o localStorage, só fazer para as funções que faltam

--- 01-fundamentos/secao-04-javascript-dom-e-seletores/dia-04-javascript-web-storage/exercicio/script.js
+++ 01-fundamentos/secao-04-javascript-dom-e-seletores/dia-04-javascript-web-storage/exercicio/script.js
@@ -1,43 +1,62 @@
-const body = document.getElementById("bodyProp");
+const main = document.querySelector("main").style;
+//O Elemento body está agora em:
+window.onload = () => {
+  //Parte de iniciar:
+  const init = () => {
+    let backgroundC = JSON.parse(localStorage.getItem("backgroundColor"));
+    if (backgroundC) main.backgroundColor = backgroundC;
+  };
 
-const changeBackgroundColor = (event) => {
-  body.style.backgroundColor = event.target.innerText;
-};
-const backgroundColor = document.querySelector("#background-color").children;
-// console.log(backgroundColor);
-for (let index of backgroundColor) {
-  index.addEventListener("click", changeBackgroundColor);
-}
+  //Essa parte terá as funções:
 
-const changeFontColor = (event) => {
-    body.style.color = event.target.innerText;
-}
-const color = document.querySelector("#color").children;
-for (let index of color) {
-    index.addEventListener("click", changeFontColor);  }
+  const changeBackgroundColor = (event) => {
+    main.backgroundColor = event.target.innerText;
+    localStorage.setItem(
+      "backgroundColor",
+      JSON.stringify(event.target.innerText)
+    );
+  };
+
+  const changeFontColor = (event) => {
+    main.color = event.target.innerText;
+    localStorage.setItem("color", JSON.stringify(event.target.innerText));
+  };
 
   const changeFontSize = (event) => {
-    body.style.fontSize = event.target.innerText;
-}
-const fontSize = document.querySelector("#font-size").children;
-for (let index of fontSize) {
-    index.addEventListener("click", changeFontSize);
-  }
+    main.fontSize = event.target.innerText;
+    localStorage.setItem("fontSize", JSON.stringify(event.target.innerText));
+  };
 
-const changeLineHeight = (event) => {
-    body.style.lineHeight = event.target.innerText;
-
-}
-const lineHeight = document.querySelector("#line-height").children;
-for (let index of lineHeight) {
-    index.addEventListener("click", changeLineHeight);
-  }
+  const changeLineHeight = (event) => {
+    main.lineHeight = event.target.innerText;
+    localStorage.setItem("lineHeight", JSON.stringify(event.target.innerText));
+  };
 
   const changeFontFamily = (event) => {
-    body.style.fontFamily = event.target.innerText;
-}
-const fontFamily = document.querySelector("#font-family").children;
-for (let index of fontFamily) {
+    main.fontFamily = event.target.innerText;
+    localStorage.setItem("fontFamily", JSON.stringify(event.target.innerText));
+  };
+
+  //Início e eventLsteners
+  const color = document.querySelector("#color").children;
+  for (let index of color) {
+    index.addEventListener("click", changeFontColor);
+  }
+  const fontSize = document.querySelector("#font-size").children;
+  for (let index of fontSize) {
+    index.addEventListener("click", changeFontSize);
+  }
+  const lineHeight = document.querySelector("#line-height").children;
+  for (let index of lineHeight) {
+    index.addEventListener("click", changeLineHeight);
+  }
+  const fontFamily = document.querySelector("#font-family").children;
+  for (let index of fontFamily) {
     index.addEventListener("click", changeFontFamily);
   }
-
+  const backgroundColor = document.querySelector("#background-color").children;
+  for (let index of backgroundColor) {
+    index.addEventListener("click", changeBackgroundColor);
+  }
+  init();
+};
