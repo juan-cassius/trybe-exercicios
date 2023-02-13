9716064ad935983c75047f09666e9a45b64110e2
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-02-13 15:04:44 -0300

    Ainda não importei tratei o date e as validações. Trocando de branch

--- 01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ 01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,4 +1,42 @@
-const picker = new Pikaday({
+import JustValidate from "just-validate";
+const validate = new window.JustValidate(
+    '#form', 
+    {
+      errorFieldCssClass: 'is-invalid',
+      errorFieldStyle: {
+        border: '1px solid red',
+      },
+      errorLabelCssClass: 'is-label-invalid',
+      errorLabelStyle: {
+        color: 'red',
+        textDecoration: 'underlined',
+      },
+      focusInvalidField: true,
+      lockForm: true,
+      tooltip: {
+        position: 'top',
+      },
+    },
+  );
+  
+  function clearFields() {
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+      const userInput = formElements[index];
+      userInput.value = '';
+      userInput.checked = false;
+    }
+    textArea.value = '';
+  };
+  
+  function enableSubmit() {
+    const submitBtn = document.querySelector('#submit-btn');
+    const agreement = document.querySelector('#agreement');
+    submitBtn.disabled = !agreement.checked;
+  }
+  
+  const picker = new Pikaday({
     field: document.getElementById('date'),
     format: 'D/M/YYYY',
     toString(date, format) {
@@ -15,3 +53,60 @@ const picker = new Pikaday({
         return new Date(year, month, day);
     }
   });
+  
+  validate
+    .addField('#fullName', [
+      {
+        rule: 'required',
+        errorMessage: 'O campo de nome é obrigatório.',
+      },
+      {
+        rule: 'maxLength',
+        value: 40,
+        errorMessage: 'O limite é de 40 caracteres.',
+      },
+      {
+        rule: 'minLength',
+        value: 10,
+        errorMessage: 'O mínimo é de 10 caracteres.',
+      },
+    ])
+    .addField('#email', [
+      {
+        rule: 'required',
+        errorMessage: 'O campo de email é obrigatório.',
+      },
+      {
+        rule: 'email',
+        errorMessage: 'O email digitado não é válido.',
+      },
+      {
+        rule: 'maxLength',
+        value: 50,
+        errorMessage: 'O limite é de 50 caracteres.',
+      },
+    ])
+    .addField('#why', [
+      {
+        rule: 'required',
+        errorMessage: 'O campo de resposta é obrigatório.',
+      },
+      {
+        rule: 'maxLength',
+        value: 500,
+        errorMessage: 'O limite é de 500 caracteres',
+      },
+    ])
+    .addField('#date', [
+      {
+        rule: 'required',
+        errorMessage: 'O campo de data é obrigatório.',
+      },
+    ]);
+  
+  window.onload = function() {
+    const clearBtn = document.querySelector('#clear-btn');
+    clearBtn.addEventListener('click', clearFields);
+    const agreement = document.querySelector('#agreement');
+    agreement.addEventListener('change', enableSubmit);
+  };
