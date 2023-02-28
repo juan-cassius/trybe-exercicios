e4c63e46fc8d63514d9914dffba07f32f45a9148
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-02-28 16:31:55 -0300

    Exercício terminado

--- 01-fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ 01-fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -1,9 +1,42 @@
 const createItem = require('./createItem');
 describe('a função createItem', () => {
-  it.todo('cria um item válido');
-  it.todo('utiliza zero como quantidade padrão');
-  it.todo('Lança um erro quando não recebe parâmetros');
-  it.todo('Lança um erro se o nome do item não é uma string');
-  it.todo('Lança um erro se o preço é negativo');
-  it.todo('Lança um erro se o preço é zero');
+  it('cria um item válido', () => {
+    const item = createItem('item', 'unidade', 5, 10);
+    expect(item).toEqual({
+      name: 'item',
+      quantity: 10,
+      unit: 'unidade',
+      price: 5,
+    });
+  });
+  it('utiliza zero como quantidade padrão', () => {
+    const item = createItem('item', 'unidade', 5);
+    expect(item).toEqual({
+      name: 'item',
+      quantity: 0,
+      unit: 'unidade',
+      price: 5,
+    });
+  });
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+  });
+  it('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(5, 'unidade', 5, 10)).toThrow();
+  });
+  it('Lança um erro se o preço do item não é um número', () => {
+    expect(() => createItem('item', 'unidade', '5', 10)).toThrow();
+  });
+  it('cria um item válido', () => {
+    const item = createItem('item', 'unidade', 5, 10);
+    expect(item).toEqual({
+      name: 'item',
+      quantity: 10,
+      unit: 'unidade',
+      price: 5,
+    });
+  });
+  it('Lança um erro se o preço do item é menor ou igual a zero', () => {
+    expect(() => createItem('item', 'unidade', 0, 10)).toThrow();
+  });
 });
