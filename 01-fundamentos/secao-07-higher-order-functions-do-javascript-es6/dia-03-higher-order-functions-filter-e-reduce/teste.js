70a018ad6b51249ead2f1c97031aef9be4c1d372
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-03-06 16:38:04 -0300

    Começando os exercícios do dia

---
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/teste.js
@@ -0,0 +1,16 @@
+const numbers = [1, 32, 44, 2, 3];
+
+const totalSum = numbers.reduce((acc, curr) => {
+  console.log(`valor do acc é ${acc} e o de curr é ${curr}`);
+   // Iteração 1: valor do acc é 30 e o de curr é 1
+   // Iteração 2:valor do acc é 31 e o de curr é 32
+   // Iteração 3: valor do acc é 63 e o de curr é 44
+   // Iteração 4: valor do acc é 107 e o de curr é 2
+   // Iteração 5: valor do acc é 109 e o de curr é 3
+   // Valor final de `acc`é 112 e `curr` para no 3
+
+
+  return acc + curr;
+}, 30); // O `valorInicial` é 30, ou seja, `acc` antes de executar a primeira iteração já está armazenando esse valor.
+
+console.log(`valor final do acc é ${totalSum}`);
