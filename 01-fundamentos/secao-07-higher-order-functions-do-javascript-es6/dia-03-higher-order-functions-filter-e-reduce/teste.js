4388964d039cf6cb116604da1b7837760f79c682
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-03-07 01:37:27 -0300

    Passando para o exercício do dia, repoitório separado

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/teste.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/teste.js
@@ -1,16 +1,22 @@
-const numbers = [1, 32, 44, 2, 3];
+// const numbers = [1, 32, 44, 2, 3];
 
-const totalSum = numbers.reduce((acc, curr) => {
-  console.log(`valor do acc é ${acc} e o de curr é ${curr}`);
-   // Iteração 1: valor do acc é 30 e o de curr é 1
-   // Iteração 2:valor do acc é 31 e o de curr é 32
-   // Iteração 3: valor do acc é 63 e o de curr é 44
-   // Iteração 4: valor do acc é 107 e o de curr é 2
-   // Iteração 5: valor do acc é 109 e o de curr é 3
-   // Valor final de `acc`é 112 e `curr` para no 3
+// const totalSum = numbers.reduce((acc, curr) => {
+//   console.log(`valor do acc é ${acc} e o de curr é ${curr}`);
+//    // Iteração 1: valor do acc é 30 e o de curr é 1
+//    // Iteração 2:valor do acc é 31 e o de curr é 32
+//    // Iteração 3: valor do acc é 63 e o de curr é 44
+//    // Iteração 4: valor do acc é 107 e o de curr é 2
+//    // Iteração 5: valor do acc é 109 e o de curr é 3
+//    // Valor final de `acc`é 112 e `curr` para no 3
 
 
-  return acc + curr;
-}, 30); // O `valorInicial` é 30, ou seja, `acc` antes de executar a primeira iteração já está armazenando esse valor.
+//   return acc + curr;
+// }, 30); // O `valorInicial` é 30, ou seja, `acc` antes de executar a primeira iteração já está armazenando esse valor.
 
-console.log(`valor final do acc é ${totalSum}`);
+// console.log(`valor final do acc é ${totalSum}`);
+
+const numbers = [50, 85, -30, 3, 15];
+
+console.log(numbers.reduce((bigger, number) => {
+    return bigger > number
+  }));
