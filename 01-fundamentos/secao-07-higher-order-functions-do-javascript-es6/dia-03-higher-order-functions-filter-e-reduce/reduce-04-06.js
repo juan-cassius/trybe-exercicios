a1a5ce7638b90e5d16dd467d8301c62dd7f27391
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-03-07 08:23:42 -0300

    Exercícios bônus terminados

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/reduce-04-06.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/reduce-04-06.js
@@ -1,7 +1,18 @@
 const names = [
-    'Aanemarie', 'Adervandes', 'Akifusa',
-    'Abegildo', 'Adicellia', 'Aladonata',
-    'Abeladerco', 'Adieidy', 'Alarucha',
-  ];
-  const expectedResult = 20;
-  const countA = () => names.reduce((acc, curr)=> acc + curr.toLowerCase().split('a').length-1,0);
+  'Aanemarie',
+  'Adervandes',
+  'Akifusa',
+  'Abegildo',
+  'Adicellia',
+  'Aladonata',
+  'Abeladerco',
+  'Adieidy',
+  'Alarucha',
+];
+const expectedResult = 20;
+const countA = () =>
+  names.reduce(
+    (acc, curr) => acc + curr.toLowerCase().split('a').length - 1,
+    0
+  );
+i;
---
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/reduce-bonus-01.js
@@ -0,0 +1,3 @@
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+
+const flatten = () => arrays.reduce((acc,curr) => acc.concat(curr));
