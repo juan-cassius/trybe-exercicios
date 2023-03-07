11d84ca224b14c811577e42fee1e40e06b863612
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-03-07 07:50:22 -0300

    REquisito 3 terminado

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/reduce.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/reduce.js
@@ -69,8 +69,17 @@ const countries = [
 const sumPop = (acc, curr) => acc + curr.population;
 const getPopulation = () => countries.reduce(sumPop, 0);
 
-const expectedResult = 4311757;
-const sumArea = (acc, curr) => acc + curr.area; 
+const sumArea = (acc, curr) => acc + curr.area;
 const getTotalArea = () => countries.reduce(sumArea, 0);
 
-console.log(getTotalArea());
+const expectedResult = {
+  name: 'American Samoa',
+  region: 'Oceania',
+  currencies: [{ code: 'USD', name: 'United States Dollar' }],
+  capital: 'Pago Pago',
+  population: 55197,
+  area: 199,
+};
+
+const compareName = (acc, curr) => acc.name.length < curr.name.length? curr: acc;
+const longestName = () => countries.reduce (compareName);
