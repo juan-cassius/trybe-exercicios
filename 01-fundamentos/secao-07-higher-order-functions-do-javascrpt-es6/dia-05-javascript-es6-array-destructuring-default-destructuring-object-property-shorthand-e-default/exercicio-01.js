f50d95b290e714d5e76896bb2604cdfa1d6ff1e6
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-03-09 00:23:25 -0300

    Exercício 2 terminado

---
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascrpt-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default/exercicio-01.js
@@ -0,0 +1,5 @@
+const myList = [5, 2, 3];
+
+const swap = ([num1,num2, num3]) => [num3,num2, num1];
+
+console.log(swap(myList));
---
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascrpt-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default/teste.js
@@ -0,0 +1,32 @@
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+
+const getPosition = (latitude, longitude) => ({
+    latitude,
+    longitude,
+  });
+  
+  console.log(getPosition(-19.8157, -43.9542));
